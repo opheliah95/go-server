59c866e37416236e64cb5232ab906dfe7d0d8fda
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2023-04-05 12:51:23 +0100

    added post method

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -29,6 +31,35 @@ func loadPage (title string) (*Page, error) {
 	return &Page{ Title: title, Body : body}, nil
 }
 
+func handler (w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "only get method is supported", http.StatusMethodNotAllowed)
+	}
+	if r.URL.Path != "/" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+	}
+	fmt.Fprintf(w, "Hi there, You entered / %s for request!", r.URL.Path[1:])
+}
+
+func formHandler (w http.ResponseWriter, r *http.Request) {
+	// only take post method
+	if r.Method != "POST" {
+		http.Error(w, "only POST method is supported", http.StatusMethodNotAllowed)
+		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "There is some error in your form liks %v", err)
+	} else{
+		fmt.Fprintf(w, "post request successful!")
+		fname := r.FormValue("fname")
+		lname:= r.FormValue("lname")
+		fmt.Fprintf(w, "first name: %s and last name is %s", fname, lname)
+	}
+	
+
+
+}
 func main() {
 	body, _ := os.ReadFile("./static/index.html")
 	page := &Page{
@@ -44,4 +75,10 @@ func main() {
 	p2, _ :=loadPage("TestPage")
 	fmt.Println(string(p2.Body))
 
+	// some file handling function
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/form", formHandler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
